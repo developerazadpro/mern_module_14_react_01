ce8c1223da0535dfb95a90bbe776acca95d0e87b
developerazadpro <azharul.ece13.hstu@gmail.com>
2023-07-16 23:06:03 +0600

    react component practice

---
+++ practice2/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './component/Header';
+import Content from './component/Content';
+import Footer from './component/Footer';
+
+const App = () => {
+  return (
+    <div>
+        <Header/>
+        <Content/>
+        <Footer/>
+    </div>
+  );
+};
+
+export default App;
---
+++ practice2/vite-project/src/component/Content.jsx
@@ -0,0 +1,9 @@
+const Content = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat cupiditate nostrum assumenda atque quibusdam in quae? Amet nemo animi saepe optio sunt obcaecati hic quidem aliquid iure, laudantium incidunt vel?</p>
+        </div>
+    )
+}
+
+export default Content;
